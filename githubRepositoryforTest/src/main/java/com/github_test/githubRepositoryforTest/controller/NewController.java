35db9559d6360e7f2a442c1b68159ad049b4c5ab
Nanninggu <[pohd8494@naver.com]>
2022-08-22 15:18:57 +0900

    dev commit Test2

--- githubRepositoryforTest/src/main/java/com/github_test/githubRepositoryforTest/controller/NewController.java
+++ githubRepositoryforTest/src/main/java/com/github_test/githubRepositoryforTest/controller/NewController.java
@@ -2,13 +2,18 @@ package com.github_test.githubRepositoryforTest.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NewController {
 
-    @GetMapping("testr/get")
-    public String testhello(){
+    @GetMapping("test/get")
+    public String testhello1(){
+        return "hello!!!!!";
+    }
+    @PostMapping("test/post")
+    public String testhello2(){
         return "hello!!!!!";
     }
 
